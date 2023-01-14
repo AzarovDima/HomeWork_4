d63df51c51756d9988b524e218b86dd5bb0e3dff
AzarovDima <ada89885308140@gmail.com>
2023-01-14 23:40:43 +0300

    Решили 2 задачу

--- challenge_1/Program.cs
+++ challenge_1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+ */
+
+int numA = GetNumbetFromUserA("Введите целоечисло А: ", " Ошибка ввода!");
+int numB = GetNumbetFromUserB("Введите целоечисло B: ", " Ошибка ввода!");
+int resultNumbers = GetResultNumbers(numA);
+Console.WriteLine($"{numA}, {numB} -> {resultNumbers}");
+
+int GetNumbetFromUserA(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+int GetNumbetFromUserB(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetResultNumbers(int num1, num2)
+{
+    int count = 1;
+    while (numB <= count)
+    {
+        numA = numA * numA;
+        count++;
+    }
+    return num;
+}
